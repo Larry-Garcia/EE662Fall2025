9cbc762aad131e543decb5fc2e15328a6990629e
Mustafa Tosun <ce_mustafatosun@hotmail.com>
2021-10-18 16:19:07 -0700

    hop_count

--- wsnlab/source/config.py
+++ wsnlab/source/config.py
@@ -7,7 +7,7 @@ BROADCAST_NODE_ADDR = 255
 
 ## node properties
 NODE_TX_RANGE = 75  # transmission range of nodes
-NODE_ARRIVAL_MAX = 100  # max time to wake up
+NODE_ARRIVAL_MAX = 50  # max time to wake up
 
 
 
@@ -16,7 +16,9 @@ NODE_ARRIVAL_MAX = 100  # max time to wake up
 ## simulation prperties
 SIM_NODE_COUNT = 100  # noce count in simulation
 SIM_NODE_PLACING_CELL_SIZE = 60  # cell size to place one node
+#SIM_DURATION = 500  # simulation Duration in seconds----original 
 SIM_DURATION = 500
+#SIM_TIME_SCALE = 0.0001  #  The real time dureation of 1 second simualtion time
 SIM_TIME_SCALE = 1
 SIM_TERRAIN_SIZE = (700, 700)  #terrain size
 SIM_TITLE = 'Data Collection Tree'  # title of visualization window
